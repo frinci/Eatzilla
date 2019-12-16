b6f2288963235ce2c3558db56f1dedf233c42b7c
frinci <frinci@gmail.com>
2019-12-16 13:55:46 -0500

    added home.jsx

---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Component from 'react-router-dom'
+
+class Home extends Component {
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Header>
+
+                </Header>
+            </Layout>
+        )
+    }
+}
+
+export default Home
