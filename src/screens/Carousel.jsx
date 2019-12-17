927dc95a464110b033d42a4d1a02ee46ea694f14
frinci <frinci@gmail.com>
2019-12-17 15:32:14 -0500

    images added to carousel, removed second node package

--- src/screens/Carousel.jsx
+++ src/screens/Carousel.jsx
@@ -1,46 +1,54 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import costco from '../images/costco.png'
+import Keyfood from '../images/Keyfood.png'
+import weis from '../images/weis.png'
+import wholefoods from '../images/wholefoods.png'
+// import '../scss/custom.scss'
 
 const CarouselContainer = () => {
   return (
+
+    <div className='deliveryPartners'>
       <Carousel>
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=First slide&bg=373940"
-    alt="First slide"
-  />
-  <Carousel.Caption>
-    <h3>First slide label</h3>
-    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=Second slide&bg=282c34"
-    alt="Third slide"
-  />
-  <Carousel.Caption>
-    <h3>Second slide label</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-  <img
-    className="d-block w-100"
-    src="holder.js/800x400?text=Third slide&bg=20232a"
-    alt="Third slide"
-  />
-  <Carousel.Caption>
-    <h3>Third slide label</h3>
-    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-  </Carousel.Caption>
-</Carousel.Item>
-</Carousel>
+        <h3 className='deliveryTitle'>Delivery Partners</h3>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={costco}
+            alt="costco logo"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Keyfood}
+            alt="keyfoods logo"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={weis}
+            alt="weis logo"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={wholefoods}
+            alt="wholefoods logo"
+          />
+        </Carousel.Item>
+
+      </Carousel>
+    </div>
   )
 }
-
 
 
 export default CarouselContainer
