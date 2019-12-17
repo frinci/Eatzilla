fe843b9b0dec408708cde98fdbcd21d11d0d49c7
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2019-12-17 16:14:44 -0500

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import './App.css';
 // import Home from './screens/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CarouselContainer from '../src/screens/Carousel'
-<<<<<<< HEAD
-import Figures from '../src/screens/Figures'
-=======
 import './scss/custom.scss'
->>>>>>> d1263e75aafae100acd9fb6ba10ef6a7ffa6052a
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Figures/>
-=======
       <head>
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet"/>
       </head>
->>>>>>> d1263e75aafae100acd9fb6ba10ef6a7ffa6052a
       <CarouselContainer />
     </div>
   );
