e1fae3521b882890ba6c7e09f25dd84ff1a59b47
frinci <frinci@gmail.com>
2019-12-17 18:16:06 -0500

    bottom complete, but needs media-queries

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <head>
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet"/>
       </head>
       <CarouselContainer />
     </div>
