e1c20fa935ac06fedef0d0cbdf8401a22df2df64
Thomas Kang <tkang9285@gmail.com>
2019-12-18 10:22:59 -0500

    pushed up header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Header from './screens/Header';
 // import Home from './screens/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CarouselContainer from '../src/screens/Carousel'
@@ -14,6 +15,7 @@ function App() {
       <head>
       <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet"/>
       </head>
+      <Header/>
       <FiguresOne/>
       <CarouselContainer />
       <FiguresTwo/>
