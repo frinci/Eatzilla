0085348a9686dad1aa43a566e82caed4cdf2ec2e
frinci <frinci@gmail.com>
2019-12-19 16:19:05 -0500

    finally centered logo

--- src/screens/Carousel.jsx
+++ src/screens/Carousel.jsx
@@ -57,11 +57,13 @@ const CarouselContainer = () => {
 
       <hr className='carouselDivider' />
 
-      <img
-        className="carouselEatZillalogo"
-        src={eatZillaLogo}
-        alt="eatZilla logo"
-      />
+      <div className='carouselLogo'>
+        <img
+          className="carouselEatZillalogo"
+          src={eatZillaLogo}
+          alt="eatZilla logo"
+        />
+      </div>
 
       <p className='loremParagraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
